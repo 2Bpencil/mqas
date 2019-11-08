d13c86aeb4ef30e69675d33a38666656fbdd716e
tyf <63250124@qq.com>
2019-11-08 17:37:15 +0800

    错题以word文档形式导出

--- src/main/java/com/tyf/mqas/utils/PoiWordUtil.java
+++ src/main/java/com/tyf/mqas/utils/PoiWordUtil.java
@@ -2,12 +2,14 @@ package com.tyf.mqas.utils;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @Description: word工具
@@ -16,7 +18,7 @@ import java.io.IOException;
  */
 public class PoiWordUtil {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 创建Word文件
         XWPFDocument doc = new XWPFDocument();
         PoiWordUtil.addPicture(doc,new File("F:\\test\\doc\\aaa"),"jpg","aaa");
@@ -25,18 +27,35 @@ public class PoiWordUtil {
         PoiWordUtil.addPicture(doc,new File("F:\\test\\doc\\bbb"),"jpg","ddd");
         PoiWordUtil.addPicture(doc,new File("F:\\test\\doc\\bbb"),"jpg","eee");
         PoiWordUtil.addPicture(doc,new File("F:\\test\\doc\\bbb"),"jpg","fff");
-        try{
-            FileOutputStream out = new FileOutputStream("F:\\test\\doc\\test.docx");
-            doc.write(out);
-            out.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        XWPFHeaderFooterPolicy policy = doc.createHeaderFooterPolicy();
+        XWPFHeader header = policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT);
+        XWPFFooter footer = policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT);
+        footer.getParagraphs();
+        List<XWPFParagraph> paragraphList = header.getParagraphs();
+        System.out.println(paragraphList.size());
+        XWPFParagraph paragraph = paragraphList.get(0);
+        paragraph.createRun().setText("Hello Header World!");
+        paragraph.createRun().setColor("CC00FF");
+        FileOutputStream out = new FileOutputStream("F:\\test\\doc\\test.docx");
+        doc.write(out);
+        out.close();
+
+
     }
 
+
+
+    /**
+    * @Description:
+    * @Param:  imgFile 图片文件   suffix图片后缀  title 描述图片
+    * @return:
+    * @Author: Mr.Tan
+    * @Date: 2019/11/8 11:16
+    */
     public static void  addPicture(XWPFDocument doc, File imgFile,String suffix,String title){
-        // 新建一个段落
+        //新建一个段落
         XWPFParagraph p = doc.createParagraph();
+        //一个XWPFRun代表具有相同属性的一个区域。
         XWPFRun r = p.createRun();
         int format = 0;
         switch ("."+suffix){
